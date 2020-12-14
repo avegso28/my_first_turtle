f640c9ccc5e9d081cc6d73de5b4a150f2c71b32e
avegso28 <69869488+avegso28@users.noreply.github.com>
2020-12-14 15:03:21 -0500

    first commit

---
+++ inputs_and_variables.py
@@ -0,0 +1,8 @@
+'''This is the first program I will write that has both inputs, variables, and multiline comments.
+12/9/20
+Alex Vegso'''
+
+
+#calculates the perimiter and area of a rectangle
+l = input("What is the length of the rectangle? ")
+w = input("What is the width of the rectangle")
