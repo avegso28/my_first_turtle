8b7bff389cd6cc9a11f1b7cfa99af538ca2687e8
avegso28 <69869488+avegso28@users.noreply.github.com>
2021-02-03 10:55:09 -0500

    made a 3 lists of numbers

--- number_list.py
+++ number_list.py
@@ -2,14 +2,14 @@ number_list_o = ["0400", "10660", "12285", "12480", "13118", "13832", "14560", "
 number_list_1 = []
 number_list_2 = []
 number_list_3 = []
-for list1 in number_list_o:
-    list1 = int(list1)
-    if (list1 % 7 == 0) and (list1 % 13 == 0):
-        number_list_3.append()
-    elif (list1 % 7 == 0):
-        number_list_1.append()
-    elif (list1 % 13 == 0):
-        number_list_2.append()
+for number in number_list_o:
+    number = int(number)
+    if (number % 7 == 0) and (number % 13 == 0):
+        number_list_3.append(number)
+    elif (number % 7 == 0):
+        number_list_1.append(number)
+    elif (number % 13 == 0):
+        number_list_2.append(number)
     else: 
         print("There is something wrong here")
 print(number_list_1)
